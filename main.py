d46d4abddff478457bad7f0707a042f135c0bbf1
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-05-26 12:53:27 +0800

    First commit

--- main.py
+++ main.py
@@ -18,5 +18,9 @@ def home():
 
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
-if __name__=="__main__":
-    app.run('0.0.0.0', port=5000)
+app.secret_key = os.urandom(12)
+    # for local deployment
+    #app.run(debug=True)
+    # for heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
