b3c999fb6b4e596de126f48954086c8f7de2da4f
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-05-26 13:02:40 +0800

    First commit

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from flask import Flask, redirect, request
 import pytesseract
 import os
+import cv2
+import shutil
+from PIL import Image
 
 app = Flask(__name__)
 
 
-def read_ocr(image):
-    pass
+def read_ocr(url):
+    r = requests.get(url, stream=True)
+    if r.status_code != 200:
+        print("Unable to retrieve image")
+        return
+    filename = f'tmp.{url[-3:]}'
+    with open(filename, 'wb+') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+    image = cv2.imread(filename)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255,
+                         cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    gray = cv2.medianBlur(gray, 3)
+    cv2.imwrite(filename, gray)
+    out = pytesseract.image_to_string(Image.open(filename))
+    print(out)
 
 
 @app.route('/', methods=['GET', 'POST'])
