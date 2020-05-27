13facf6973d4cdb1c9437fda495e169a9b5b84a6
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-05-27 20:15:46 +0800

    Updated data flow

---
+++ image.py
@@ -0,0 +1,73 @@
+import pytesseract
+from PIL import Image
+import os
+import face_recognition as fr
+import numpy as np
+import cv2
+
+
+def read_ocr_plain():
+    text = pytesseract.image_to_string(Image.open('gray'))
+    text = text.replace('|', 'I')
+    text = text.replace('\n', ' ')
+    text = text.replace('[', '')
+    return text
+
+
+def get_encoded_faces():
+    """
+    looks through the faces folder and encodes all
+    the faces
+
+    :return: dict of (name, image encoded)
+    """
+    encoded = {}
+
+    for dirpath, dnames, fnames in os.walk("./faces"):
+        for f in fnames:
+            if f.endswith(".jpg") or f.endswith(".png"):
+                face = fr.load_image_file("faces/" + f)
+                encoding = fr.face_encodings(face)[0]
+                encoded[f.split(".")[0]] = encoding
+
+    return encoded
+
+
+FACES = get_encoded_faces()
+def classify_faces_plain():
+    """
+    will find all of the faces in a given image and label
+    them if it knows what they are
+
+    :param im: str of file path
+    :return: list of face names
+    """
+    global FACES
+    faces = FACES
+    img = cv2.imread('raw', 1)
+    print('ok')
+    if img is None:
+        print("Invalid image array")
+        return
+
+    faces_encoded = list(faces.values())
+    known_face_names = list(faces.keys())
+
+    face_locations = fr.face_locations(img)
+    unknown_face_encodings = fr.face_encodings(img, face_locations)
+
+    face_names = []
+    for face_encoding in unknown_face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = fr.compare_faces(faces_encoded, face_encoding)
+        name = "Unknown"
+
+        # use the known face with the smallest distance to the new face
+        face_distances = fr.face_distance(faces_encoded, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+        face_names.append(name)
+
+    return face_locations, face_names
--- main.py
+++ main.py
@@ -1,79 +1,10 @@
 from flask import Flask, redirect, request, flash
-import pytesseract
-from PIL import Image
 import os
-import face_recognition as fr
-import numpy as np
-import cv2
+
+from image import read_ocr_plain, classify_faces_plain
+from direct import get_img, read_ocr, classify_faces
 
 app = Flask(__name__)
-
-
-def read_ocr():
-    text = pytesseract.image_to_string(Image.open('gray'))
-    text = text.replace('|', 'I')
-    text = text.replace('\n', ' ')
-    text = text.replace('[', '')
-    return text
-
-
-def get_encoded_faces():
-    """
-    looks through the faces folder and encodes all
-    the faces
-
-    :return: dict of (name, image encoded)
-    """
-    encoded = {}
-
-    for dirpath, dnames, fnames in os.walk("./faces"):
-        for f in fnames:
-            if f.endswith(".jpg") or f.endswith(".png"):
-                face = fr.load_image_file("faces/" + f)
-                encoding = fr.face_encodings(face)[0]
-                encoded[f.split(".")[0]] = encoding
-
-    return encoded
-
-FACES = get_encoded_faces()
-
-def classify_faces():
-    """
-    will find all of the faces in a given image and label
-    them if it knows what they are
-
-    :param im: str of file path
-    :return: list of face names
-    """
-    global FACES
-    faces = FACES
-    img = cv2.imread('raw', 1)
-    print('ok')
-    if img is None:
-        print("Invalid image array")
-        return
-
-    faces_encoded = list(faces.values())
-    known_face_names = list(faces.keys())
-
-    face_locations = fr.face_locations(img)
-    unknown_face_encodings = fr.face_encodings(img, face_locations)
-
-    face_names = []
-    for face_encoding in unknown_face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = fr.compare_faces(faces_encoded, face_encoding)
-        name = "Unknown"
-
-        # use the known face with the smallest distance to the new face
-        face_distances = fr.face_distance(faces_encoded, face_encoding)
-        best_match_index = np.argmin(face_distances)
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-
-        face_names.append(name)
-
-    return face_locations, face_names
 
 
 
@@ -84,12 +15,27 @@ def home():
         request.files['raw'].save('raw')
         request.files['gray'].save('gray')
         print(f'File size: {os.path.getsize("raw")}')
-        text = read_ocr()
+        text = read_ocr_plain()
         res = text
-        res += '!^@*!%@^#*!@^#'+str(classify_faces())
+        res += '!^@*!%@^#*!@^#'+str(classify_faces_plain())
         return res
 
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
+@app.route('/direct', methods=['GET', 'POST'])
+def direct():
+    if request.method == 'POST':
+        url = request.form.get('url')
+        ext = get_img(url)  # saved to 'raw'+ext
+        print(f'File size: {os.path.getsize("raw"+ext)}')
+        text = read_ocr(ext)
+        res = text
+        res += '!^@*!%@^#*!@^#'+str(classify_faces(ext))
+        return res
+
+    return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
 
 if __name__=="__main__":
     app.secret_key = os.urandom(12)
