ff8257e107b984f0dfd9b49754e3f1e291de8547
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-06-10 16:01:12 +0800

    Added more debugging info

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def direct():
 		res.append(read_qr())
 		print(res[1])
 		res.append(classify_faces())
-		print(res[2]
+		print(res[2])
 		res = list(map(str, res))
 		return '*#&%^@'.join(res)
 		
